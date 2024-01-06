65c6791bfdd489635170bb3860fce93224fa21e5
amarjeet-saini <amarjeets167@gmail.com>
2024-01-06 15:57:45 +0200

    added more func. to Vec2 class

--- include/GameEngine/Vec2.hpp
+++ include/GameEngine/Vec2.hpp
@@ -7,12 +7,25 @@
 
 class Vec2 {
  public:
-  float x, y;
+  float x, y;  // x,y component of vector
+
   Vec2();
   Vec2(float xin, float yin);
-  bool operator==(const Vec2& v);  // compare two vectors
-  Vec2 operator+(const Vec2& v);   // add two vectors
-  Vec2 operator*(const Vec2& v);   // multiply two vectors
-  float distance(const Vec2& v);   // norm between two vectors
-  Vec2& add(const Vec2& v); // v = v + v1
+
+  bool operator==(const Vec2 &v) const;  // v1 equal v2 ?
+  bool operator!=(const Vec2 &v) const;  // v1 not equal v2 ?
+
+  Vec2 operator+(const Vec2 &v) const;    // v = v1 + v2
+  Vec2 operator-(const Vec2 &v) const;    // v = v1 - v2
+  Vec2 operator*(const float val) const;  // v = v1 * val
+  Vec2 operator/(const float val) const;  // v = v1 / val
+
+  void operator+=(const Vec2 &v);  // v = v + v1
+  void operator-=(const Vec2 &v);  // v = v - v1
+  void operator*=(const Vec2 &v);  // v = v * v1
+  void operator/=(const Vec2 &v);  // v = v / v1
+
+  float dist(const Vec2 &v) const;  // distance between two vector
+  float length() const;             //  magnitude of vector
+  void normalize();                 // normalize vector
 };
--- src/GameEngine/Vec2.cpp
+++ src/GameEngine/Vec2.cpp
@@ -1,22 +1,78 @@
-#include "GameEngine/Vec2.hpp"
+#include "Vec2.hpp"
 #include <cmath>
 
 Vec2::Vec2() : x(0), y(0) {}
 
 Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {}
 
-bool Vec2::operator==(const Vec2& v) { return (x == v.x) && (y == v.y); }
-
-Vec2 Vec2::operator+(const Vec2& v) { return Vec2(x + v.x, y + v.y); }
-
-Vec2 Vec2::operator*(const Vec2& v) { return Vec2(x * v.x, y * v.y); }
-
-float Vec2::distance(const Vec2& v) {
-  return sqrt((x - v.x)*(x - v.x) + (y - v.y)*(y - v.y));
+// Equality operator
+bool Vec2::operator==(const Vec2 &rhs) const {
+  return (x == rhs.x) && (y == rhs.y);
 }
 
-Vec2& Vec2::add(const Vec2& v) {
+// Inquality operator
+bool Vec2::operator!=(const Vec2 &rhs) const {
+  return (x != rhs.x) || (y != rhs.y);
+}
+
+// Addition of vector
+Vec2 Vec2::operator+(const Vec2 &rhs) const {
+  return Vec2(x + rhs.x, y + rhs.y);
+}
+
+// Subtraction of vector
+Vec2 Vec2::operator-(const Vec2 &rhs) const {
+  return Vec2(x - rhs.x, y - rhs.y);
+}
+
+// Scalar multiplication
+Vec2 Vec2::operator*(const float val) const { return Vec2(x * val, y * val); }
+
+// Scalar division
+Vec2 Vec2::operator/(const float val) const {
+  // avoid division by zero by checking if val is not zero
+  return (val != 0 ? Vec2(x / val, y / val) : *this);
+}
+
+// Compound addition assignment operator
+void Vec2::operator+=(const Vec2 &v) {
   x += v.x;
   y += v.y;
-  return *this;
+}
+
+// Compound subtraction assignment operator
+void Vec2::operator-=(const Vec2 &v) {
+  x -= v.x;
+  y -= v.y;
+}
+
+// Compound multiplication assignment operator
+void Vec2::operator*=(const Vec2 &v) {
+  x *= v.x;
+  y *= v.y;
+}
+
+// Compound division assignment operator
+void Vec2::operator/=(const Vec2 &v) {
+  if (v.x != 0.0f && v.y != 0.0f) {
+    x /= v.x;
+    y /= v.y;
+  }
+}
+
+// Distance between two vector
+float Vec2::dist(const Vec2 &v) const {
+  return sqrt((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y));
+}
+
+// Calculate the Euclidean length (magnitude) of the vector
+float Vec2::length() const { return sqrt((x * x) + (y * y)); }
+
+// Normalize the vector to make its length (magnitude) equal to 1
+void Vec2::normalize() {
+  float len = length();
+  if (len != 0.0f) {
+    x /= len;
+    y /= len;
+  }
 }
